24023b61ddfd906b4f30cd521024a5a2617b6b94
buiduyhien <hien.bd@seesaa.vn>
2020-06-27 12:53:50 +0700

    use TimbreLayout

--- Timbre/TutorialsViewController.swift
+++ Timbre/TutorialsViewController.swift
@@ -18,8 +18,9 @@ class TutorialsViewController: UICollectionViewController {
     
     collectionView!.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
     
-    let layout = collectionViewLayout as! UICollectionViewFlowLayout
+    let layout = TimbreLayout()
     layout.itemSize = CGSize(width: collectionView!.bounds.width, height: 140)
+    collectionView.collectionViewLayout = layout
   }
   
 }
